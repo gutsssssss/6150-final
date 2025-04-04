923fe383bd7e29c8309220333d86205ca884ada9
gutsssssss <95394016+gutsssssss@users.noreply.github.com>
2025-04-04 15:06:38 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DateTimePicker from "./DateTimePicker";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <DateTimePicker />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = (props) => {
+    return (
+        <h1>
+            Welcome to our website, {props.name}!
+        </h1>
+    );
+}
+
+export default Title;
